4aeb7e034478c731be02e5a22dfce2859f0285d5
caio.schnepper <caio.schnepper@sistemairis.com.br>
2023-12-14 15:01:24 -0300

    feat: melhoria no'router'

--- App/Controllers/Router.php
+++ App/Controllers/Router.php
@@ -14,17 +14,22 @@ class Router
 
     public function __construct($page = NULL, $id = NULL)
     {
-        if($page == NULL){
-            $this->$page = new Main();
-        } elseif ($page == 'cadastrar-feedback'){
-            $this->$page = new CadastrarFeedback($_SERVER['PHP_SELF']);
-        } elseif ($page == 'lista-feedback'){
-            $db = Conexao::get_instance();
-            $data = $db->find('feedback');
-            $this->$page = new ListarFeedback($data);
-        } elseif ($page == 'feedback'){
-            $p = new Feedback($id);
-            $this->page = new \App\Views\Feedback($_SERVER['PHP_SELF'],$p);
+        switch ($page) {
+            case 'cadastrar-feedback':
+                $this->$page = new CadastrarFeedback($_SERVER['PHP_SELF']);
+                break;
+            case 'lista-feedback':
+                $db = Conexao::get_instance();
+                $data = $db->find('feedback');
+                $this->$page = new ListarFeedback($data);
+                break;
+            case 'feedback':
+                $feedback = new Feedback($id);
+                $this->page = new \App\Views\Feedback($_SERVER['PHP_SELF'], $feedback);
+                break;
+            default:
+                $this->$page = new Main();
+                break;
         }
     }
 }
